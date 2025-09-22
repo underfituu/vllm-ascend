5511a0107a53ea9fab5a9720aece6acca1c99524
underfituu <hzhucong@163.com>
2025-09-22 11:36:24 +0800

    1
    Signed-off-by: underfituu <hzhucong@163.com>

--- collect_env.py
+++ collect_env.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Adapted from https://github.com/vllm-project/vllm/blob/main/collect_env.py
-#
+#123
 
 import datetime
 import locale
